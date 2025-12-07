263574c8878bdf7e0346aaeaa1dd51d4be6c6a67
Aravind <gmarav005@gmail.com>
2025-12-07 20:20:10 +0530

    Add states

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,47 @@
 import '../index.css'
 import image from '../assets/image.png'
+import { useState,useEffect } from 'react'
 
 export default function Quiz() {
+
+    const [questions, setQuestions] = useState([])
+    const [isGameOver, setIsGameOver] = useState(false)
+    const [data, setData] = useState(null)
+
+    useEffect(function() {
+        fetch("https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple")
+        .then(res => res.json())
+        .then(data => {
+           
+            const updatedQuestions = data.results.map((question) => {
+                const allAnswers = [...questions.incorrect_answers]
+
+                const randomIndex = Math.floor(
+                    Math.random() * (allAnswers.length + 1)
+                );
+
+                allAnswers.splice(randomIndex, 0, question.correct_answers)
+
+                return {
+                    ...questions,
+                    ...allAnswers,
+                    selectedAnswerIndex: 1,
+                }
+
+            });
+             setData(updatedQuestions)
+        })
+
+    },[])
+
+    
+
+
     return (
     <main>
         <img src={image}/>
+        <p>{JSON.stringify(data)}</p>
+        
 
 
 
